b86db9b356195fdfd1732b92b390634b2fca5f7a
priscian <priscian@gmail.com>
2016-09-28 12:00:48 -0400

    Data update; corrections to 'plot_horse_race()'.

--- R/helper.R
+++ R/helper.R
@@ -86,8 +86,14 @@ plot_horse_race <- function(series, top_n_years=NULL, baseline=TRUE)
     d3 <- d3[year %in% d4$year, ]
   }
 
+  baselineText <- ""
+  if (is.logical(baseline)) {
+    if (baseline) baseline <- defaultBaseline
+  }
+  if (!is.null(baseline))
+    baselineText <- " w.r.t. " %_% min(baseline) %_% "\u2013" %_% max(baseline)
+
   subtitle <- paste(series, " ", min(d$year), "\u2013", max(d$year), sep="")
-  baselineText <- " w.r.t. " %_% 1981 %_% "\u2013" %_% 2010
   ylab <- eval(substitute(expression(paste("Temperature Anomaly (", phantom(l) * degree, "C)", b, sep="")), list(b=baselineText)))
   g <- ggplot(d3, aes(x=month, y=`YTD mean temp.`, group=factor(year), color=factor(year))) +
     geom_line() +
